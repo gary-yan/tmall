a3f1675fd41da2adf27cba8620d343e8b37857c7
gary-yan <383791970@qq.com>
2018-05-22 18:35:48 +0200

    Dao class degign #1

---
+++ src/tmall/bean/CategoryDao.java
@@ -0,0 +1,16 @@
+package tmall.bean;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import tmall.bean.Category;
+import tmall.util.DBUtil;
+
+public class CategoryDao {
+
+}
