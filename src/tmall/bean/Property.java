a2c0eb6aa9fb7d138e04cbcc93cdbbed7d629b8d
gary-yan <383791970@qq.com>
2018-07-18 22:13:34 +0200

    property and DateUtil

--- src/tmall/bean/Property.java
+++ src/tmall/bean/Property.java
@@ -1,21 +1,17 @@
 package tmall.bean;
 
-public class Property {
+public class Property{
 	private int id;
 	private Category category;
 	private String name;
-	public int getId() {
+	
+	public int getId(){
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	public Category getCategory() {
-		return category;
-	}
-	public void setCategory(Category category) {
-		this.category = category;
-	}
+	
 	public String getName() {
 		return name;
 	}
@@ -23,4 +19,71 @@ public class Property {
 		this.name = name;
 	}
 	
+	public Category category() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//public class Property {
+//	private int id;
+//	private Category category;
+//	private String name;
+//	public int getId() {
+//		return id;
+//	}
+//	public void setId(int id) {
+//		this.id = id;
+//	}
+//	public Category getCategory() {
+//		return category;
+//	}
+//	public void setCategory(Category category) {
+//		this.category = category;
+//	}
+//	public String getName() {
+//		return name;
+//	}
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//	
+//}
--- src/tmall/util/DateUtil.java
+++ src/tmall/util/DateUtil.java
@@ -11,4 +11,4 @@ public class DateUtil {
 			return null;
 		return new java.util.Date(t.getTime());
 	}
-}
+}//DateUtil这个日期工具类主要是用于java.util.Date类与java.sql.Timestamp 类的互相转换。
