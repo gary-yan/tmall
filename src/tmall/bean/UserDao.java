179fb133c724b71b57e2cc681773671c5e256ebb
gary-yan <383791970@qq.com>
2018-07-21 01:54:51 +0200

    UserDao

--- src/tmall/bean/UserDao.java
+++ src/tmall/bean/UserDao.java
@@ -30,13 +30,35 @@ public class UserDao {
 		try(Connection c = DBUtil.getConnection(); PreparedStatement ps= c.prepareStatement(sql);){
 			ps.setString(1, bean.getName());
 			ps.setString(2, bean.getPassowrd());
-			ResultSet rs = ps.getGeneratedKeys();
+			ResultSet rs = ps.getGeneratedKeys();//想获得刚插入记录的ID，可以用getGeneratedKeys获得insert插入后生成的主键ID
 			if(rs.next()) {
 				int id = rs.getInt(1);
 				bean.setId(id);
 			}
 			}catch(SQLException e) {
 				e.printStackTrace();
+		}
+	}
+	public void update(User bean) {
+		
+		String sql = "update user set name = ?, password =? where id = ?";
+		try(Connection c = DBUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql);){
+			
+			ps.setString(1, bean.getName());
+			ps.setString(2, bean.getPassowrd());
+			ps.setInt(3, bean.getId());
+			
+			ps.execute();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public void delete(int id) {
+		try(Connection c = DBUtil.getConnection(); Statement s = c.createStatement();){
+			String sql = "delete from User wehre id = " + id;
+			s.execute(sql);
+		}catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
 	}
