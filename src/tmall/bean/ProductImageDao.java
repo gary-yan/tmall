42b6e34efac26885bc4f7fe536754d1da30f08df
gary-yan <383791970@qq.com>
2018-07-30 14:53:26 +0200

    0730

--- src/tmall/bean/ProductImageDao.java
+++ src/tmall/bean/ProductImageDao.java
@@ -1,5 +1,51 @@
 package tmall.bean;
 
-public class ProductImageDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import tmall.bean.Product;
+import tmall.bean.ProductImage;
+import tmall.util.DBUtil;
+
+public class ProductImageDao {
+	
+	public static final String type_single = "type_single";
+	public static final String type_detail = "type_detail";
+	
+	public int getTotal() {
+		int total = 0;
+		try(Connection c = DBUtil.getConnection(); Statement s = c.createStatement();){
+			String sql = "select count(*) from ProductImage";
+			
+			ResultSet rs = s.executeQuery(sql);
+			while(rs.next()) {
+				total = rs.getInt(1);
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return total;
+	}
+	
+	public void add(ProductImage bean) {
+		String sql = "insert into ProductImage values(null,?,?)";
+		try(Connection c = DBUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql);){
+			ps.setInt(1,bean.getProduct().getId());
+			ps.setString(2, bean.getType());
+			ps.execute();
+			
+			ResultSet rs = ps.getGeneratedKeys();
+			if(rs.next()) {
+				int id = rs.getInt(1);
+				bean.setId(id);
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/tmall/bean/PropertyDao.java
+++ src/tmall/bean/PropertyDao.java
@@ -14,7 +14,7 @@ import tmall.bean.Product;
 import tmall.bean.Property;
 import tmall.util.DBUtil;
 import tmall.util.DateUtil;
-
+import tmall.bean.CategoryDao;
 public class PropertyDao {
 	
 	public int getTotal(int cid) {
@@ -63,4 +63,79 @@ public class PropertyDao {
 			e.printStackTrace();
 		}
 	}
-}
+	 public void delete(int id) {
+		  
+	        try (Connection c = DBUtil.getConnection(); Statement s = c.createStatement();) {
+	  
+	            String sql = "delete from Property where id = " + id;
+	  
+	            s.execute(sql);
+	  
+	        } catch (SQLException e) {
+	  
+	            e.printStackTrace();
+	        }
+	    }
+	  
+	    public Property get(int id) {
+	        Property bean = new Property();
+	  
+	        try (Connection c = DBUtil.getConnection(); Statement s = c.createStatement();) {
+	  
+	            String sql = "select * from Property where id = " + id;
+	  
+	            ResultSet rs = s.executeQuery(sql);
+	  
+	            if (rs.next()) {
+	 
+	                String name = rs.getString("name");
+	                int cid = rs.getInt("cid");
+	                bean.setName(name);
+	                Category category = new CategoryDao().get(cid);
+	                bean.setCategory(category);
+	                bean.setId(id);
+	            }
+	  
+	        } catch (SQLException e) {
+	  
+	            e.printStackTrace();
+	        }
+	        return bean;
+	    }
+	  
+	    public List<Property> list(int cid) {
+	        return list(cid, 0, Short.MAX_VALUE);
+	    }
+	  
+	    public List<Property> list(int cid, int start, int count) {
+	        List<Property> beans = new ArrayList<Property>();
+	  
+	        String sql = "select * from Property where cid = ? order by id desc limit ?,? ";
+	  
+	        try (Connection c = DBUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql);) {
+	  
+	            ps.setInt(1, cid);
+	            ps.setInt(2, start);
+	            ps.setInt(3, count);
+	  
+	            ResultSet rs = ps.executeQuery();
+	  
+	            while (rs.next()) {
+	                Property bean = new Property();
+	                int id = rs.getInt(1);
+	                String name = rs.getString("name");
+	                bean.setName(name);
+	                Category category = new CategoryDao().get(cid);
+	                bean.setCategory(category);
+	                bean.setId(id);
+	                 
+	                beans.add(bean);
+	            }
+	        } catch (SQLException e) {
+	  
+	            e.printStackTrace();
+	        }
+	        return beans;
+	    }
+	  
+	}
