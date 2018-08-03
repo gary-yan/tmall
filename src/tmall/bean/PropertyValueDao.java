b444eac10b7a52607330e311c5d4754e6a018e20
gary-yan <383791970@qq.com>
2018-08-03 21:30:19 +0200

    PropertyValueDao

--- src/tmall/bean/PropertyValueDao.java
+++ src/tmall/bean/PropertyValueDao.java
@@ -1,5 +1,71 @@
 package tmall.bean;
+//package tmall.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+ 
+import tmall.bean.Product;
+import tmall.bean.Property;
+import tmall.bean.PropertyValue;
+import tmall.util.DBUtil;
 
 public class PropertyValueDao {
-
+	public int getTotal() {
+		int total = 0;
+		try(Connection c = DBUtil.getConnection(); Statement s = c.createStatement(); ){
+			String sql = "select count(*) from PropertyValue" ;
+			ResultSet rs = s.executeQuery(sql);
+			while(rs.next()) {
+				total =rs.getInt(1);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return total;
+	}
+	
+	public void add(PropertyValue bean) {
+		String sql = "insert into Property values (null,?,?,?)";
+		try(Connection c =DBUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql);){
+			ps.setInt(1, bean.getProduct().getId());
+			ps.setInt(2,bean.getProperty().getId());
+			ps.setString(3, bean.getValue());
+			ps.execute();
+			ResultSet rs = ps.getGeneratedKeys();
+			if(rs.next()) {
+				int id = rs.getInt(1);
+				bean.setId(id);//这个id是propertyvalue的id?
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void update(PropertyValue bean) {
+		String sql = "update propertyvalue set pid = ?, ptid = ?,value = ? where id = ? ";
+		try(Connection c = DBUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql);){
+			ps.setInt(1, bean.getProduct().getId());
+			ps.setInt(2, bean.getProperty().getId());
+			ps.setString(3, bean.getValue());
+			ps.execute();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void delete(int id) {
+		try(Connection c = DBUtil.getConnection(); Statement s = c.createStatement();){
+			String sql = "delete from propertyvalue where id =" + id;
+			s.execute(sql);
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 }
